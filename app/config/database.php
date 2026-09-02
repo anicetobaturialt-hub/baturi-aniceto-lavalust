d5f7519559f896109b19f9e1160f625a88d1feaa
anicetobaturialt-hub <anicetobaturialt@gmail.com>
2026-09-02 21:28:40 +0800

    Configure database via environment variables for Aiven

--- app/config/database.php
+++ app/config/database.php
@@ -58,14 +58,14 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> '',
-    'hostname'	=> getenv('localhost') ?: '',
-    'port'		=> getenv('3306') ?: '',
-    'username'	=> getenv('root') ?: '',
-    'password'	=> getenv('') ?: '',
-    'database'	=> getenv('mydb') ?: '',
-    'charset'	=> '',
-    'dbprefix'	=> '',
+    'driver'    => 'mysqli',
+    'hostname'  => getenv('DB_HOST') ?: 'localhost',
+    'port'      => getenv('DB_PORT') ?: '3306',
+    'username'  => getenv('DB_USERNAME') ?: 'root',
+    'password'  => getenv('DB_PASSWORD') ?: '',
+    'database'  => getenv('DB_DATABASE') ?: 'mydb',
+    'charset'   => 'utf8mb4',
+    'dbprefix'  => '',
     // Optional for SQLite
     'path'      => ''
 );
