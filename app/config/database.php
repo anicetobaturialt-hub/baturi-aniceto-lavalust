6586c024ca1ae03dbd42a044c8f9c94cec25b9d3
anicetobaturialt-hub <anicetobaturialt@gmail.com>
2026-09-02 22:44:38 +0800

    remove the i

--- app/config/database.php
+++ app/config/database.php
@@ -58,7 +58,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'    => 'mysqli',
+    'driver'    => 'mysql',
     'hostname'  => getenv('DB_HOST') ?: 'localhost',
     'port'      => getenv('DB_PORT') ?: '3306',
     'username'  => getenv('DB_USERNAME') ?: 'root',
